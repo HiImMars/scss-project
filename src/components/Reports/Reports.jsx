c921de78d60db9e286e4e3be66aa0447d8d45591
HiImMars <fedorenkorostyslav@gmail.com>
2024-02-22 22:29:11 +0200

    reports

--- src/components/Reports/Reports.jsx
+++ src/components/Reports/Reports.jsx
@@ -1,25 +1,33 @@
 import { RxAvatar } from "react-icons/rx";
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
+import { BsDash } from "react-icons/bs";
 
 const reports = [
   {
     name: "Andrew Mike",
     progress: "89",
+    status: "up",
   },
   {
-    name: "Rostyslav Fedorenko",
+    name: "Rostyslav",
     progress: "96",
+    status: "dash",
   },
   {
     name: "John Doe",
     progress: "73",
+    status: "down",
   },
   {
     name: "Jake Sullivan",
     progress: "65",
+    status: "up",
   },
   {
     name: "Mila Mitchell",
     progress: "99",
+    status: "dash",
   },
 ];
 
@@ -30,17 +38,16 @@ const Reports = () => {
         <span>Task Report</span> | Last 2 Weeks
       </h2>
       <ul className="reports-list">
-        {reports.map(({ name, progress }) => (
-          <li className="reports-item" key={name}>
-            <div className="reports-co-worker co-worker">
-              <div className="co-worker-thumb">
-                <RxAvatar className="co-worker-avatar" width="32" height="32" />
-              </div>
-              <h3 className="co-worker-caption">{name}</h3>
+        {reports.map(({ name, progress, status }) => (
+          <li className="reports-item" key={name} data-status={status}>
+            <div className="reports-staff staff">
+              <RxAvatar className="staff-avatar" />
+
+              <h3 className="staff-caption">{name}</h3>
             </div>
             <progress
               className="reports-bar"
-              id="reports_ibnu"
+              id="reports_indash"
               max="100"
               value={progress}
             >
@@ -48,6 +55,11 @@ const Reports = () => {
                 <span>Progress: {progress}%</span>
               </div>
             </progress>
+            {status === "up" && <IoIosArrowUp className="reports-icon up" />}
+            {status === "dash" && <BsDash className="reports-icon dash" />}
+            {status === "down" && (
+              <IoIosArrowDown className="reports-icon down" />
+            )}
           </li>
         ))}
       </ul>
